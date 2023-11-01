555cae9bd115fbab48c59cff4308e2aa11e7b8d8
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2023-11-01 11:32:51 +0700

    recyclerView

--- app/src/main/java/com/hendro/androidkotlinbindingbasicui/RecyclerViewActivity.kt
+++ app/src/main/java/com/hendro/androidkotlinbindingbasicui/RecyclerViewActivity.kt
@@ -2,10 +2,23 @@ package com.hendro.androidkotlinbindingbasicui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hendro.androidkotlinbindingbasicui.databinding.ActivityRecyclerViewBinding
 
 class RecyclerViewActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRecyclerViewBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_recycler_view)
+        binding = ActivityRecyclerViewBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        //back button
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
+
     }
 }
